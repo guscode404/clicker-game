e76349e44bfac87116d9f5b5efe4824e650d3011
Gustavo <gustavokingu0@gmail.com>
2022-09-06 15:24:15 -0300

    Contador de clicks adicionado

--- clicar.js
+++ clicar.js
@@ -1,8 +1,14 @@
 var btnClick = document.getElementById("btn-click");
 var dinheiro = Number(document.getElementById("dinheiro").innerHTML);
 var spanDinheiro = document.getElementById("dinheiro");
+var clicks = Number(document.getElementById("clicks-totais").innerHTML);
+var spanClicks = document.getElementById("clicks-totais");
+console.log(clicks);
 
 btnClick.addEventListener("click", function() {
     dinheiro += 1;
     spanDinheiro.innerHTML = dinheiro;
+
+    clicks += 1;
+    spanClicks.innerHTML = clicks;
 })
